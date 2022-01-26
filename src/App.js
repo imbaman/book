f9500c176b81e650be269f5b4319cf8cfb7a14d5
imbaman <imbaman2@gmail.com>
2022-01-26 21:08:59 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,53 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/react";
+import { useState } from "react";
+import { Dialog } from "@reach/dialog";
+import "@reach/dialog/styles.css";
+
 function App() {
-  return <div className='App'>app</div>;
+  const [showDialog, setShowDialog] = useState("none");
+
+  return (
+    <div
+      css={{
+        backgroundImage: "linear-gradient(135deg, #fdfcfb 0%, #e2d1c3 100%)",
+        height: "100vh",
+      }}>
+      <div
+        css={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          height: "100%",
+        }}>
+        <h1>Logo</h1>
+        <div>
+          <button onClick={() => setShowDialog("login")}>Log in</button>
+          <button onClick={() => setShowDialog("register")}>Sign up</button>
+        </div>
+
+        <Dialog
+          onDismiss={() => setShowDialog("false")}
+          isOpen={showDialog === "login"}
+          aria-label='Login form'
+          style={{ boxShadow: "0px 10px 50px hsla(0, 0%, 0%, 0.33)" }}>
+          <p>login</p>
+          <button onClick={() => setShowDialog("none")}>1</button>
+          <button onClick={() => setShowDialog("none")}>2</button>
+        </Dialog>
+        <Dialog
+          onDismiss={() => setShowDialog("false")}
+          isOpen={showDialog === "register"}
+          aria-label='Register form'
+          style={{ boxShadow: "0px 10px 50px hsla(0, 0%, 0%, 0.33)" }}>
+          <p>register</p>
+          <button onClick={() => setShowDialog("none")}>1</button>
+          <button onClick={() => setShowDialog("none")}>2</button>
+        </Dialog>
+      </div>
+    </div>
+  );
 }
 
 export default App;
