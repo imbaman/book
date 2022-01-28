0fcd5f93b5186aa38f570bc0ebeb4d2c5e147e80
imbaman <imbaman2@gmail.com>
2022-01-28 10:41:58 +0100

    lib updated

--- src/components/lib.js
+++ src/components/lib.js
@@ -1,8 +1,28 @@
 /** @jsx jsx */
-import { jsx } from "@emotion/react";
-
+import { jsx, keyframes } from "@emotion/react";
+import * as colors from "../styles/colors";
 import styled from "@emotion/styled";
+import Dialog from "@reach/dialog";
+import { ImSpinner9 } from "react-icons/im";
+//media queries
+const small = "@media (max-width:991px)";
 
+const spin = keyframes({
+  "0%": { transform: "rotate(0deg)" },
+  "100%": { transform: "rotate(360deg)" },
+});
+const Spinner = styled(ImSpinner9)({
+  animation: `${spin} 1s linear infinite`,
+});
+
+const CustomDialog = styled(Dialog)({
+  boxShadow: "0px 10px 50px hsla(0, 0%, 0%, 0.33)",
+  padding: "1rem",
+  [small]: {
+    width: "100%",
+    margin: "10vh 0",
+  },
+});
 const Button = styled.button(
   {
     // fontSize: "20px",
@@ -12,8 +32,18 @@ const Button = styled.button(
     border: "1px solid black",
     cursor: "pointer",
     backgroundColor: "rgba(0, 0, 0, 0)",
+    ":hover": {
+      backgroundColor: colors.yellow,
+    },
   },
   (props) => ({ backgroundColor: props.color })
 );
 
-export { Button };
+const Input = styled.input({
+  borderRadius: "1px",
+  border: "1px solid #f4f4f4",
+  background: colors.yellow,
+  padding: "8px 12px",
+});
+
+export { Button, Input, CustomDialog, Spinner };
