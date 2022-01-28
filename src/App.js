5ad68b9ea7d381e2ba9d5e2d5f316b841b44cab6
imbaman <imbaman2@gmail.com>
2022-01-28 14:32:01 +0100

    changes for future auth

--- src/App.js
+++ src/App.js
@@ -1,52 +1,14 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/react";
-import { useState } from "react";
-import { Dialog } from "@reach/dialog";
-import "@reach/dialog/styles.css";
-
+import React from "react";
+import BooksForYou from "./BooksForYou";
+import Unauthenticated from "./Unauthenticated";
 function App() {
-  const [showDialog, setShowDialog] = useState("none");
-
   return (
-    <div
-      css={{
-        backgroundImage: "linear-gradient(135deg, #fdfcfb 0%, #e2d1c3 100%)",
-        height: "100vh",
-      }}>
-      <div
-        css={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-          height: "100%",
-        }}>
-        <h1>Logo</h1>
-        <div>
-          <button onClick={() => setShowDialog("login")}>Log in</button>
-          <button onClick={() => setShowDialog("register")}>Sign up</button>
-        </div>
-
-        <Dialog
-          onDismiss={() => setShowDialog("false")}
-          isOpen={showDialog === "login"}
-          aria-label='Login form'
-          style={{ boxShadow: "0px 10px 50px hsla(0, 0%, 0%, 0.33)" }}>
-          <p>login</p>
-          <button onClick={() => setShowDialog("none")}>1</button>
-          <button onClick={() => setShowDialog("none")}>2</button>
-        </Dialog>
-        <Dialog
-          onDismiss={() => setShowDialog("false")}
-          isOpen={showDialog === "register"}
-          aria-label='Register form'
-          style={{ boxShadow: "0px 10px 50px hsla(0, 0%, 0%, 0.33)" }}>
-          <p>register</p>
-          <button onClick={() => setShowDialog("none")}>1</button>
-          <button onClick={() => setShowDialog("none")}>2</button>
-        </Dialog>
-      </div>
-    </div>
+    <>
+      {/* <Unauthenticated /> */}
+      <BooksForYou />
+    </>
   );
 }
 
---
+++ src/Unauthenticated.js
@@ -0,0 +1,95 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/react";
+
+import { useState } from "react";
+import "@reach/dialog/styles.css";
+import { Button, Input, CustomDialog, Spinner } from "./components/lib";
+
+function LoginForm({ buttonText, onSubmit }) {
+  function handleSubmit(e) {
+    e.preventDefault();
+    const { username, password } = e.target.elements;
+
+    onSubmit({
+      username: username.value,
+      password: password.value,
+    });
+  }
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit}
+        css={{
+          // display: "flex",
+          // flexDirection: "column",
+          // alignItems: "stretch",
+          "> div": { margin: "10px auto", maxWidth: "350px", width: "100%" },
+        }}>
+        <div css={{ display: "flex", flexDirection: "column" }}>
+          <label htmlFor='username'>Username</label>
+          <Input id='username' />
+        </div>
+        <div css={{ display: "flex", flexDirection: "column" }}>
+          <label htmlFor='password'>Password</label>
+          <Input id='password' type='password' />
+        </div>
+        <div>
+          <Button type='submit'>{buttonText}</Button>
+          <Spinner />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+function Unauthenticated() {
+  const [showDialog, setShowDialog] = useState("none");
+
+  function login(formData) {
+    console.log("login", formData);
+  }
+  function register(formData) {
+    console.log("register", formData);
+  }
+  return (
+    <div
+      css={{
+        backgroundImage: "linear-gradient(135deg, #fdfcfb 0%, #e2d1c3 100%)",
+        height: "100vh",
+      }}>
+      <div
+        css={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          height: "100%",
+        }}>
+        <h1>Logo</h1>
+        <div css={{ display: "flex" }}>
+          <Button onClick={() => setShowDialog("login")}>Log In</Button>
+          <Button color='white' onClick={() => setShowDialog("register")}>
+            Sign up
+          </Button>
+        </div>
+
+        <CustomDialog
+          onDismiss={() => setShowDialog("false")}
+          isOpen={showDialog === "login"}
+          aria-label='Login form'>
+          <p>login</p>
+          <LoginForm buttonText={"login"} onSubmit={login} />
+        </CustomDialog>
+        <CustomDialog
+          onDismiss={() => setShowDialog("false")}
+          isOpen={showDialog === "register"}
+          aria-label='Register form'>
+          <p>register</p>
+          <LoginForm buttonText={"register"} onSubmit={register} />
+        </CustomDialog>
+      </div>
+    </div>
+  );
+}
+
+export default Unauthenticated;
