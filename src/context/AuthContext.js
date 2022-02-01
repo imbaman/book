61289f0b010fc9346b11d72766447eb4a46296b1
imbaman <imbaman2@gmail.com>
2022-02-01 12:32:43 +0100

    auth context

---
+++ src/context/AuthContext.js
@@ -0,0 +1,41 @@
+import React, { createContext, useContext, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { auth } from "../firebase";
+
+const AuthContext = createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState();
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+
+  const signup = async (auth, email, password) => {
+    try {
+      const user = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(user);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const logout = async () => {
+    await signOut(auth);
+  };
+
+  const value = {
+    user,
+    signup,
+    logout,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
