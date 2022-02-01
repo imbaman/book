84d2d7bc42233d6525a7785fb2054740e9a4b3df
imbaman <imbaman2@gmail.com>
2022-02-01 12:34:04 +0100

    auth

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import { jsx } from "@emotion/react";
 import React from "react";
 import BooksForYou from "./BooksForYou";
 import Unauthenticated from "./Unauthenticated";
+import { useAuth } from "./context/AuthContext";
+
 function App() {
-  return (
-    <>
-      {/* <Unauthenticated /> */}
-      <BooksForYou />
-    </>
-  );
+  const { user } = useAuth();
+  console.log(user);
+  return user ? <BooksForYou /> : <Unauthenticated />;
 }
 
 export default App;
