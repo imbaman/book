07c340ed567149491a9155bf5e7122a40bb33239
imbaman <imbaman2@gmail.com>
2022-02-10 20:41:42 +0100

    firebase db integration

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,9 +1,26 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import { db } from "../firebase";
+import { addDoc, collection } from "@firebase/firestore";
 
-const Book = () => {
+const Book = (book) => {
   const [data, setData] = useState("");
   const { bookId } = useParams();
+  const { user } = useAuth();
+  const bookCollectionRef = collection(db, "bookList");
+
+  const addBook = async () => {
+    await addDoc(bookCollectionRef, {
+      bookId: bookId,
+      ownerId: user.uid,
+      author: data?.volumeInfo?.authors,
+      title: data?.volumeInfo?.title,
+      desc: data?.volumeInfo?.description,
+      img: data?.volumeInfo?.imageLinks?.thumbnail,
+      // book: data.volumeInfo,
+    });
+  };
   useEffect(() => {
     const getData = async () => {
       const data = await fetch(
@@ -24,6 +41,11 @@ const Book = () => {
       <img src={data?.volumeInfo?.imageLinks?.thumbnail} alt='' />
       <h1>{data?.volumeInfo?.title}</h1>
       <p>{data?.volumeInfo?.description.replace(/(<([^>]+)>)/gi, "")}</p>
+      {/* <button
+        onClick={() => {
+          console.log(user.uid, "<-uid", "bookId ->", bookId);
+        }}> */}
+      <button onClick={addBook}>add to fav</button>
     </div>
   );
 };
