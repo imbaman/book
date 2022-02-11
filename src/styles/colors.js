fca64b49b2699b4a213b75f8d8ba100e618da5a7
imbaman <imbaman2@gmail.com>
2022-02-11 09:25:23 +0100

    colors

---
+++ src/styles/colors.js
@@ -0,0 +1,8 @@
+export const yellow = "#ffe55c";
+export const main = "#222831";
+export const earth = "#EFE2BA";
+export const green = "#00887A";
+export const blue = "#77a6f7";
+export const lightBlue = "#d3e3fc";
+export const bg = "#c5cbe3";
+export const honey = "#DCAE1d";
