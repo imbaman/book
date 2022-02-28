2b14ce01531216c76667e41125b46958814be5e5
imbaman <imbaman2@gmail.com>
2022-02-28 15:28:20 +0100

    upd stars

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { FaStar } from "react-icons/fa";
-const Stars = ({ updateScore, data, updateStar }) => {
+const Stars = ({ updateScore, data, updateStar, index }) => {
   const [ratingValue, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
   useEffect(() => {
     setRating(data?.rating);
   }, []);
+
+  useEffect(() => {
+    // setRating(index);
+    //   updateStar(index);
+    updateScore(data.id, data.rating);
+    console.log(ratingValue, "rating value");
+  }, [ratingValue]);
+
   return (
     <div>
       {[...Array(5)].map((star, index) => {
@@ -18,12 +26,11 @@ const Stars = ({ updateScore, data, updateStar }) => {
             className={index <= (hover || ratingValue) ? "on" : "off"}
             onClick={() => {
               setRating(index);
-              updateStar(ratingValue);
-              updateScore(data.id, data.rating);
+              updateStar(index);
             }}
             onMouseEnter={() => setHover(index)}
             onMouseLeave={() => setHover(ratingValue)}>
-            <FaStar />
+            <FaStar size={25} />
           </button>
         );
       })}
