b2ddc04e125281a5d9d73d67f035439a439bf38e
imbaman <imbaman2@gmail.com>
2022-03-02 20:55:56 +0100

    css

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,6 +1,6 @@
 export const yellow = "#ffe55c";
 export const main = "#222831";
-export const earth = "#EFE2BA";
+export const earth = "#EB8B2D";
 export const green = "#00887A";
 export const blue = "#77a6f7";
 export const lightBlue = "#d3e3fc";
