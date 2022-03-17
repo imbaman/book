0e893c37a48333c338a788641db23952e4fbbfef
imbaman <imbaman2@gmail.com>
2022-03-17 21:56:39 +0100

    removing some comments

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -3,7 +3,7 @@ import { jsx } from "@emotion/react";
 
 import React, { useState, useEffect } from "react";
 import { FaStar } from "react-icons/fa";
-const Stars = ({ updateScore, data, updateStar, index }) => {
+const Stars = ({ updateScore, data, updateStar, index, updatePage }) => {
   const [ratingValue, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
@@ -12,15 +12,12 @@ const Stars = ({ updateScore, data, updateStar, index }) => {
   }, []);
 
   useEffect(() => {
-    // setRating(index);
-    //   updateStar(index);
     updateScore(data.id, data.rating);
     console.log(ratingValue, "rating value");
   }, [ratingValue]);
 
   return (
     <div css={{ display: "flex", alignItems: "center", marginBottom: "12px" }}>
-      <small>rate this book :</small>
       <div>
         {[...Array(5)].map((star, index) => {
           index += 1;
@@ -37,7 +34,6 @@ const Stars = ({ updateScore, data, updateStar, index }) => {
               onMouseEnter={() => setHover(index)}
               onMouseLeave={() => setHover(ratingValue)}>
               <FaStar size={25} />
-              {/* stroke: "#ff9400", strokeWidth: "5"  */}
             </button>
           );
         })}
